5309b12367d5e2036ae079be5bb9f709321e006f
Andrés Sanz Buezo <asb@bim-y.com>
2021-06-13 23:15:44 +0200

    Refactor Modal component
    - Remove modal windows for each image
    - Add Click Listener to handle which image is shown
    - Add a generic modal

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# lamarturaleza.github.io
+# la marturaleza
+
+This project wants to cover all the key areas this company is working with.
+
+The main webpage works as non dynamic page.
--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,16 @@
 M.AutoInit();
 
+const imgModal = document.getElementById("img-modal");
 
+/**
+ * This function will handle the click event and will look for the _src_ attribute in the DOM element triggered
+ * to update the _src_ attribute in the modal window.
+ * 
+ * @param {*} event 
+ */
+function onClickHandler(event) {
+    console.log("onClickHandler...");
+    const target = event.target;
+    const imgPath = target.src;
+    imgModal.src = imgPath;
+}
